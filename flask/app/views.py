1fe746ef42144a9e804102c6adee00ba94219a13
Maycon Pinheiro Campos <mayconpinheirocampos@gmail.com>
2022-02-19 04:21:42 -0300

    corrigido texto no arquivo views.py

--- flask/app/views.py
+++ flask/app/views.py
@@ -2,4 +2,4 @@ from app import app
 
 @app.route("/")
 def index():
-    return "<h1>Aqui uma página legal pra você!! =) https://github.com/MayconCampos</h1>"
+    return "<h1>Aqui uma página legal pra você!! =) https://github.com/MayconPCampos</h1>"
